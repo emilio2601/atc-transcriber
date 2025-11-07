8ca07aa7ada2543cc2584ac05958a69c5942ed54
Emilio Mendoza <emilio2601@gmail.com>
2025-11-07 15:50:41 -0500

    set up solidqueue

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,16 @@
 Rails.application.routes.draw do
+  namespace :api do
+    namespace :asr do
+      post "next",   to: "jobs#next_job"
+      post "result", to: "jobs#submit_result"
+    end
 
+    # Public-ish API for your UI
+    resources :clips, only: [ :index, :show, :update ]
+  end
 
-  # SPA shell
+  mount MissionControl::Jobs::Engine, at: "/jobs"
+
   root "spa#index"
   get "*path", to: "spa#index", constraints: ->(req) { !req.xhr? && req.format.html? }
 end
